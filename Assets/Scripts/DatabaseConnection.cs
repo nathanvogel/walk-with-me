59df7564552d832323d6f8f7a60a997ab25c781d
Nathan Vogel <nathan@nathanvogel.com>
2018-03-24 21:24:30 +0100

    :sparkles: Filter the people by room and expose the list
    The list of persons whose footprints should be displayed is now available for other scripts to use.
    See the example in FootprintManager
    
    A Manager object has been created to centralize management of the app.
    Database operations are managed separately by DatabaseConnection in the Database object

--- Assets/Scripts/DatabaseConnection.cs
+++ Assets/Scripts/DatabaseConnection.cs
@@ -11,43 +11,75 @@ public class PersonData {
     public Vector3 pos;
     public Vector3 rot;
     public string id;
-	public string roomId;
+	public string locationId;
 
-    PersonData(Vector3 pos, Vector3 rot) {
+	PersonData(Vector3 pos, Vector3 rot, string locationId) {
         this.pos = pos;
         this.rot = rot;
         id = SystemInfo.deviceUniqueIdentifier;
-		this.roomId = "ecal";
+		this.locationId = locationId;
     }
 
-    // convenience
-    public static PersonData CreatePersonData(Transform t) {
-        return new PersonData(t.position, t.rotation.eulerAngles);
+    // Convenience
+	public static PersonData CreatePersonData(Transform t, string locationId) {
+		return new PersonData(t.position, t.rotation.eulerAngles, locationId);
     }
 }
 
+
 public class DatabaseConnection : MonoBehaviour {
 
-	// A Firebase Reference to the list of person objects.
-	private DatabaseReference peopleRef;
+	// --- Data behavior settings ---
 	// Controls whether objects are deleted from Firebase when the user
 	// quits the application.
-	bool deleteWhenLeaving = false;
+	private bool deleteWhenLeaving = true;
+	private bool filterRooms = true;
+
+	// --- Firebase ---
+	// A Firebase Reference to the list of person objects.
+	private DatabaseReference peopleRef;
+
+	// Room IDs
+	public string[] rooms = {"parsons", "ecal"};
+	public string deviceLocationId; // Where the user is.
+	public string otherLocationId; // Where the user isn't.
+
+	// List of people whose footprint we need to show.
+	public Dictionary<string, PersonData> persons = new Dictionary<string, PersonData>();
 
 
     void Start() {
 		// To be called before any other Firebase function
         FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://newp-f426c.firebaseio.com/");
 
+		// Set in which room the user is.
+		// This could be determined by:
+		// - Recognition of the origin object/QR code in AR
+		// - GPS coordinates
+		// - User choice
+		// - ...
+		int roomIndex = Random.Range(0, 2);
+		deviceLocationId = rooms[roomIndex];
+		otherLocationId = rooms [roomIndex == 0 ? 1 : 0];
+		Debug.Log ("The user is in " + deviceLocationId);
+
 		// Start listening for people 
 		peopleRef = FirebaseDatabase.DefaultInstance.GetReference("people");
-		peopleRef.ChildAdded += HandleChildAdded;
-		peopleRef.ChildChanged += HandleChildChanged;
-		peopleRef.ChildRemoved += HandleChildRemoved;
+		// Filter the users to only get the data from the other room.
+		var query = peopleRef.OrderByChild("locationId");
+		if (filterRooms) {
+			query = query.EqualTo (otherLocationId);
+			// If we later want to show footprints from more than 2 rooms, we could use this technic:
+			// https://stackoverflow.com/questions/39195191/firebase-query-to-exclude-data-based-on-a-condition/39195551#39195551
+			// Or we could structure the data by room: /rooms/<roomId>/people/<personId>
+		}
+		query.ChildAdded += HandleChildAdded;
+		query.ChildChanged += HandleChildChanged;
+		query.ChildRemoved += HandleChildRemoved;
 
+		// Get ready to delete the people object if they quit the application,
+		// cutting the connection to Firebase and triggering OnDisconnect().
 		if (deleteWhenLeaving) {
-			// Get ready to delete the people object if they quit the application,
-			// cutting the connection to Firebase and triggering OnDisconnect().
 			string id = SystemInfo.deviceUniqueIdentifier;
 			peopleRef.Child (id).OnDisconnect ().RemoveValue ();
 		}
@@ -56,20 +88,6 @@ public class DatabaseConnection : MonoBehaviour {
 		StartCoroutine(_SendData());
 	}
 
-
-	// Called when the Unity Scene is closed.
-	void OnDestroy() {
-		// Turn off Firebase listener to clean up the app.
-		peopleRef.ChildAdded -= HandleChildAdded;
-		peopleRef.ChildChanged -= HandleChildChanged;
-		peopleRef.ChildRemoved -= HandleChildRemoved;
-
-		if (deleteWhenLeaving) { 
-			// Also remove the people object here to ensure immediate effect.
-			string id = SystemInfo.deviceUniqueIdentifier;
-			peopleRef.Child (id).RemoveValueAsync ();
-		}
-	}
 
 
 	// This is function is called:
@@ -82,9 +100,10 @@ public class DatabaseConnection : MonoBehaviour {
 			return;
 		}
 
-		// Do something with the data in args.Snapshot
+		// Instanciate the Object from the Firebase JSON data and add it to our array.
 		PersonData person = JsonUtility.FromJson<PersonData>(args.Snapshot.GetRawJsonValue());
-		Debug.Log (person.pos.x);
+		Debug.Log ("CREATE " + person.id);
+		persons.Add(person.id, person);
 	}
 
 	// This function is called when an object (already handled in HandleChildAdded)
@@ -96,9 +115,10 @@ public class DatabaseConnection : MonoBehaviour {
 			return;
 		}
 
-		// Get data
+		// Get new data
 		PersonData person = JsonUtility.FromJson<PersonData>(args.Snapshot.GetRawJsonValue());
-		Debug.Log (person.pos.x);
+		Debug.Log ("UPDATE " + person.id);
+		persons[person.id] = person;
 
 	}
 
@@ -110,17 +130,18 @@ public class DatabaseConnection : MonoBehaviour {
 			return;
 		}
 
-		// Do something with the data in args.Snapshot
+		Debug.Log ("REMOVE " + args.Snapshot.Key);
+		persons.Remove(args.Snapshot.Key);
 	}
 
 
-    // Coroutine
+    // Coroutine to save the user's position to Firebase
     IEnumerator _SendData() {
         while(true) {
 			// The ID that identifies the person in the room.
 			string id = SystemInfo.deviceUniqueIdentifier;
 
-            PersonData cd = PersonData.CreatePersonData(Camera.main.transform);
+			PersonData cd = PersonData.CreatePersonData(Camera.main.transform, deviceLocationId);
             string scd = JsonUtility.ToJson(cd);
 
 			// Save to Firebase
@@ -128,8 +149,30 @@ public class DatabaseConnection : MonoBehaviour {
 			//Debug.Log (cd.pos.x);
 
             // Wait half a second
-            yield return new WaitForSeconds(0.5f);
+            yield return new WaitForSeconds(2.5f);
         }
     }
 
+
+
+
+
+
+	// Called when the Unity Scene is closed.
+	void OnDestroy() {
+		/*
+		// This isn't really needed and peopleRef is already null in OnDestroy
+		// (which causes an error) so it probably belong somewhere else, if at all necessary.
+		// Turn off Firebase listener to clean up the app.
+		peopleRef.ChildAdded -= HandleChildAdded;
+		peopleRef.ChildChanged -= HandleChildChanged;
+		peopleRef.ChildRemoved -= HandleChildRemoved;
+		*/
+
+		if (deleteWhenLeaving) { 
+			// Also remove the people object here to ensure immediate effect.
+			string id = SystemInfo.deviceUniqueIdentifier;
+			peopleRef.Child (id).RemoveValueAsync ();
+		}
+	}
 }
---
+++ Assets/Scripts/FootprintManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FootprintManager : MonoBehaviour
+{
+
+	public DatabaseConnection data;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		// We can access the persons in the other room with data.persons
+
+		// For debug purposes 
+		if (Time.frameCount % 30 == 0) {
+			Debug.Log (data.persons.Count);
+		}
+	}
+}
+
