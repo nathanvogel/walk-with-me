7d081753554f4a05e082d13761ed34c0307d66bf
Maelle Chenaux <chenauxmaelle@gmail.com>
2018-04-20 09:51:42 +0200

    Import tests from other project in ./Assets/VisualTests

---
+++ Assets/VisualTests/DestroyFootprintTimer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyFootprintTimer : MonoBehaviour {
+
+    // Use this for initialization
+    public float destroyTimer;
+	void Start () {
+        Destroy(gameObject, destroyTimer);
+       
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/VisualTests/FootprintGenerator.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+	
+public class FootprintGenerator : MonoBehaviour {
+
+    public GameObject footstepPrefab;
+    public float footstepLength;
+    public float footstepOffset;
+ 
+    private bool left;
+    // Use this for initialization
+    private Vector3 lastPos;
+    void Start () {
+        lastPos = gameObject.transform.position;
+		
+	}
+	//int Quaternion = transformRotation;
+
+	// Update is called once per frame
+	void Update () {
+		//transformRotation = Quaternion(transform.rotation.x, -transform.rotation.y, transform.rotation.z, 0.0);
+
+        if (!((lastPos.magnitude+footstepLength)>gameObject.transform.position.magnitude)|| !((lastPos.magnitude - footstepLength) < gameObject.transform.position.magnitude)) {
+           
+            print("spawn dem steppy steps");
+            Vector3 stepPos;
+            //update last pos
+            lastPos = gameObject.transform.position;
+            if (left)
+            {
+                print("left bob");
+                stepPos = new Vector3(lastPos.x + footstepOffset, lastPos.y, lastPos.z);
+               
+                left = false;
+            }
+            else {
+                print("right bob");
+                stepPos = new Vector3(lastPos.x - footstepOffset, lastPos.y, lastPos.z);
+                left = true;
+            }
+            Instantiate(footstepPrefab, stepPos, Quaternion.identity);
+
+        }
+		
+	}
+}
