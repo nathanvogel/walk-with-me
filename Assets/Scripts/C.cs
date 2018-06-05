e9f8d60be7d819c8c98b6bfc0b3c890c537e2035
Nathan Vogel <nathan@nathanvogel.com>
2018-06-05 23:25:40 +0200

    Change app icon and colors

--- Assets/Scripts/C.cs
+++ Assets/Scripts/C.cs
@@ -3,5 +3,5 @@
 public class C  
 {
 	public static Color DEFAULT_FOOTPRINT_COLOR = new Color(0, 0, 0);
-	public static Color APP_COLOR = new Color(15f/255, 60f/255, 175f/255);
+	public static Color APP_COLOR = new Color(47f/255, 36f/255, 173f/255);
 }
